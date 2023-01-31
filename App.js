02f7982c207265a0f5bc546268e3416eeac11687
winnie_the_pooh <qwerwf>
2023-01-31 21:13:02 +0500

    added auth and registration

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import 'react-native-gesture-handler'
 import React from 'react'
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
 import { ThemeProvider } from 'styled-components/native'
+import { getApps, initializeApp } from 'firebase/app'
+import 'react-native-gesture-handler'
 
 import { useFonts as useOswald, Oswald_400Regular } from '@expo-google-fonts/oswald'
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato'
@@ -9,9 +10,21 @@ import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato'
 import { theme } from './src/infrastructure/theme'
 import { Navigation } from './src/infrastructure/navigation'
 
-import { RestaurantsContextProvider } from './src/services/restaurants/restaurants.context'
-import { LocationContextProvider } from './src/services/location/location.context'
-import { FavouritesContextProvider } from './src/services/favourites/favourites.context'
+import { AuthenticationContextProvider } from './src/services/authentication/authentication.context'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAG_uN631rLMYwLf9K7pUq_URO3OWBGviU',
+  authDomain: 'mealstogo-82f6a.firebaseapp.com',
+  projectId: 'mealstogo-82f6a',
+  storageBucket: 'mealstogo-82f6a.appspot.com',
+  messagingSenderId: '444458840224',
+  appId: '1:444458840224:web:59032ae629d2cc44c359c5',
+  measurementId: 'G-L688LWDC6T'
+}
+
+if (!getApps().length) {
+  initializeApp(firebaseConfig)
+}
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -29,13 +42,9 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <FavouritesContextProvider>
-          <LocationContextProvider>
-            <RestaurantsContextProvider>
-              <Navigation />
-            </RestaurantsContextProvider>
-          </LocationContextProvider>
-        </FavouritesContextProvider>
+        <AuthenticationContextProvider>
+          <Navigation />
+        </AuthenticationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
+import React, { useContext } from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Ionicons } from '@expo/vector-icons'
-import { Text } from 'react-native'
+import { Text, Button } from 'react-native'
 
 import { SafeArea } from '@app/components/utility/safe-area.component'
 
 import { RestaurantsNavigator } from './restaurants.navigator'
 import { MapScreen } from '@app/features/map/screens/map.screen'
+
+import { AuthenticationContext } from '@app/services/authentication/authentication.context'
+import { RestaurantsContextProvider } from '@app/services/restaurants/restaurants.context'
+import { LocationContextProvider } from '@app/services/location/location.context'
+import { FavouritesContextProvider } from '@app/services/favourites/favourites.context'
 
 const Tab = createBottomTabNavigator()
 
@@ -17,11 +21,15 @@ const TabIcons = {
   Settings: 'md-settings'
 }
 
-const Settings = () => (
-  <SafeArea>
-    <Text>Settings</Text>
-  </SafeArea>
-)
+const Settings = () => {
+  const { onLogout } = useContext(AuthenticationContext)
+  return (
+    <SafeArea>
+      <Text>Settings</Text>
+      <Button title="logout" onPress={() => onLogout()} />
+    </SafeArea>
+  )
+}
 
 const createScreenOptions = ({ route }) => {
   const iconName = TabIcons[route.name]
@@ -34,11 +42,15 @@ const createScreenOptions = ({ route }) => {
 }
 
 export const AppNavigator = () => (
-  <NavigationContainer>
-    <Tab.Navigator screenOptions={createScreenOptions}>
-      <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
-      <Tab.Screen name="Map" component={MapScreen} />
-      <Tab.Screen name="Settings" component={Settings} />
-    </Tab.Navigator>
-  </NavigationContainer>
+  <FavouritesContextProvider>
+    <LocationContextProvider>
+      <RestaurantsContextProvider>
+        <Tab.Navigator screenOptions={createScreenOptions}>
+          <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
+          <Tab.Screen name="Map" component={MapScreen} />
+          <Tab.Screen name="Settings" component={Settings} />
+        </Tab.Navigator>
+      </RestaurantsContextProvider>
+    </LocationContextProvider>
+  </FavouritesContextProvider>
 )
